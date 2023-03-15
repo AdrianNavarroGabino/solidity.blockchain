37c8eeecf4fbe214c29408bc0247c550b773dda3
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-15 21:26:22 +0100

    Tipos de variables y modificadores

---
+++ 10-fundamentosSmartContractsSolidity/2_variables_modifiers.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract variables_modifiers {
+
+    // valores enteros sin signos (uint)
+    uint256 a;
+    uint8 public b = 3;
+
+    // Valores enteros con signo (int)
+    int256 c;
+    int8 public d = -32;
+    int e = 65;
+
+    // Variable string
+    string str;
+    string public str_public = "Esto es publico";
+    string private str_private = "Esto es privado";
+
+    // Variable booleana
+    bool boolean;
+    bool public boolean_true = true;
+    bool private boolean_false = false;
+
+    // Variable bytes
+    bytes32 first_bytes;
+    bytes4 second_bytes;
+    bytes1 bytes_1;
+
+    // Algoritmo de hash
+    bytes32 public hashing = keccak256(abi.encodePacked("Hola", "Adrian"));
+
+    // Variable address
+    address my_address;
+    address public address1 = 0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7;
+    address public address2 = msg.sender;
+
+    // Variable de enumeracion
+    enum options {ON, OFF}
+    options state;
+    options constant defaultChoice = options.OFF;
+
+    function turnOn() public {
+        state = options.ON;
+    }
+
+    function turnOff() public {
+        state = options.OFF;
+    }
+
+    function displayState() public view returns (options) {
+        return state;
+    }
+}
