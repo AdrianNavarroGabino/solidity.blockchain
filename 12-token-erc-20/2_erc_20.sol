d9ecb4657a52187fe3df0e391c75c62405da77c1
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-19 11:14:00 +0100

    Funciones básicas ERC-20

--- 12-token-erc-20/2_erc_20.sol
+++ 12-token-erc-20/2_erc_20.sol
@@ -19,3 +19,39 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
+contract ERC20 is IERC20 {
+
+    mapping(address => uint256) private balances;
+    // Owner -> Spender -> Tokens
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return balances[account];
+    }
+}
