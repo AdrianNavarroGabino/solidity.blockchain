a5d39602c1ced7c7631b837a34010309ff9ab995
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-20 09:01:25 +0100

    Hooks del ERC-20

--- 12-token-erc-20/2_erc_20.sol
+++ 12-token-erc-20/2_erc_20.sol
@@ -146,4 +146,8 @@ contract ERC20 is IERC20 {
             }
         }
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {}
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual {}
 }
