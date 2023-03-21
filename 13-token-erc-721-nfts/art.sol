dd15de9f9059742f3bdcc6cd84fdd6de26cda2e0
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-21 21:31:57 +0100

    Proyecto ERC-721: Definiciones iniciales

--- 13-token-erc-721-nfts/art.sol
+++ 13-token-erc-721-nfts/art.sol
@@ -5,5 +5,35 @@ import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
 
 contract ArtToken is ERC721, Ownable {
+    // ===================================================
+    // Initial Statements
+    // ===================================================
 
+    // Smart Contract Constructor
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    // NFT token counter
+    uint256 COUNTER;
+
+    // Pricing of NFT Tokens (price of the artwork)
+    uint256 fee = 5 ether;
+
+    // Data structure with the properties of the artwork
+    struct Art {
+        string name;
+        uint256 id;
+        uint256 dna;
+        uint8 level;
+        uint8 rarity;
+    }
+
+    // Storage structure for keeping artworks
+    Art[] public art_works;
+
+    // Declaration of an event
+    event NewArtWork(address indexed owner, uint256 id, uint256 dna);
+
+    // ===================================================
+    // Help Functions
+    // ===================================================
 }
