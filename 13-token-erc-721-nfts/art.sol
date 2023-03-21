29f483721e5ff73c33aa28cf288f0fb093691997
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-21 21:26:24 +0100

    Proyecto ERC-721: Construcción del Smart Contract

---
+++ 13-token-erc-721-nfts/art.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
+
+contract ArtToken is ERC721, Ownable {
+
+}
